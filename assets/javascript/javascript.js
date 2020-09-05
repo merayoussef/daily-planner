7f7a549face4ac8663088e181f32c0fe8f83adcd
Mera Youssef <merayoussef91@hotmail.com>
2020-09-04 22:08:33 -0500

    styled time slots

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,4 +4,11 @@ var displayDate = document.getElementById('currentDay');
 displayDate.innerHTML = nowMoment.format("dddd, MMMM Do YYYY")
 
 
+var tasks = {};
 
+var createTask = function(){
+    var taskText = document.createElement("li")
+    var saveTask = document.createElement("button")
+
+    taskText.appendChild("nine")
+}
