24b0b86d7bdbdc325619fb8f646a852f3bb4ebbd
Mera Youssef <merayoussef91@hotmail.com>
2020-09-05 17:05:06 -0500

    updated layout, added background color and saving function

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,12 +3,48 @@ var nowMoment = moment();
 var displayDate = document.getElementById('currentDay');
 displayDate.innerHTML = nowMoment.format("dddd, MMMM Do YYYY")
 
+$(document).ready(function() {
+    // listen for save button
+    $(".saveBtn").on("click", function() {
+        //get value
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-var tasks = {};
+        //save to local storage
+        localStorage.setItem(time, value);
+    });
 
-var createTask = function(){
-    var taskText = document.createElement("li")
-    var saveTask = document.createElement("button")
+    function hourUpdate() {
+        var currentHour = moment().hours();
 
-    taskText.appendChild("nine")
-}
+        $(".time-block").each(function(){
+            var blockHour = parseInt($(this).attr("id").split
+            ("-")[1]);
+
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+            } else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+
+    hourUpdate();
+
+    var interval = setInterval(hourUpdate, 15000);
+
+    $("#nine .description").val(localStorage.getItem("nine"));
+    $("#ten .description").val(localStorage.getItem("ten"));
+    $("#eleven .description").val(localStorage.getItem("eleven"));
+    $("#twelve .description").val(localStorage.getItem("twelve"));
+    $("#one .description").val(localStorage.getItem("one"));
+    $("#two .description").val(localStorage.getItem("two"));
+    $("#three .description").val(localStorage.getItem("three"));
+    $("#four .description").val(localStorage.getItem("four"));
+    $("#five .description").val(localStorage.getItem("five"));
+});
